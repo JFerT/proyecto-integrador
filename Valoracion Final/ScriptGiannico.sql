c59a5dd4d0eacfc45fcbb035408fc2e11b3496c8
Valentino Giannico <valentinogiannico4@gmail.com>
2022-08-11 15:06:17 -0300

    nuevos cambios

--- Valoracion Final/ScriptGiannico.sql
+++ Valoracion Final/ScriptGiannico.sql
@@ -1,6 +1,6 @@
+/* CREACION DE LA BASE DE DATOS Y SUS TABLAS */
 CREATE DATABASE peluqueria_canina;
 
-/* PUNTOS 1 Y 2 */
 CREATE TABLE Dueno(
 	DNI INTEGER NOT NULL PRIMARY KEY,
     Nombre varchar(250) NOT NULL,
@@ -9,7 +9,18 @@ CREATE TABLE Dueno(
     Direccion varchar(250) NOT NULL
 );
 
-INSERT INTO Dueno VALUES (44653004, 'Valentino', 'Giannico', 3517724563, 'Padres Salesianos');
+CREATE TABLE Historial(
+	ID_Historial INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Fecha date NOT NULL,
+    Perro INTEGER NOT NULL,
+    Descripcion varchar(250) NOT NULL,
+    Monto INT NOT NULL,
+    
+    FOREIGN KEY(Perro) REFERENCES Perro(ID_Perro)
+)
+
+
+/* PUNTOS 1 Y 2 */
 
 CREATE TABLE Perro(
 	ID_Perro INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -20,6 +31,10 @@ CREATE TABLE Perro(
     
     FOREIGN KEY(DNI_dueno) REFERENCES Dueno(DNI)
 );
+
+
+INSERT INTO Dueno VALUES (44653004, 'Valentino', 'Giannico', 3517724563, 'Padres Salesianos');
+
 
 INSERT INTO Perro(Nombre, Fecha_nac, Sexo, DNI_dueno) VALUES ('Teo', '2010-04-24', 'M', 44653004);
 
