0f17665e948ad35578988512d9adc4d42b36d30e
Facundo Manuel Díaz Córdoba <95236196+facumd@users.noreply.github.com>
2022-08-13 13:27:45 -0300

    Agrega Ejercicio 7 Python

---
+++ Valoración Final/Script Py - Ej 7 - Valoración Final.py
@@ -0,0 +1,23 @@
+# 7)  Crear una tupla en Python con el nombre de “Historial3” la cual contenga los siguientes valores:
+
+#          9530, 4120, 4580, 1500, 890,7516,426
+
+# Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Lennon”. 
+# Calcular el promedio de gasto en pesos por atención de “Lennon” mostrándolo en pantalla. 
+# Si el promedio es mayor a 4500 indicar con un mensaje “Se ha excedido del gasto promedio para su mascota”.
+
+Historial3 = (9530, 4120, 4580, 1500, 890, 7516, 426);
+
+suma_montos = 0
+
+for i in Historial3:
+    suma_montos = suma_montos + i
+
+cantidad_elementos = len(Historial3)
+
+promedio_gastos = int(suma_montos / cantidad_elementos)
+
+if promedio_gastos > 4500:
+    print(f'Se ha excedido del gasto promedio para su mascota. Su promedio es de: ${promedio_gastos}')
+else:
+    print(f'Su promedio es de: ${promedio_gastos}')
