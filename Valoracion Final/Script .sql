6ead3d425c5c09904497e6e8d9e958df9004c445
Pulpoi <pablo37942814@gmail.com>
2022-08-13 12:29:45 -0300

    se agrego script.sql de la valoracion final

---
+++ Valoracion Final/Script .sql
@@ -0,0 +1,43 @@
+create database peluqueria;
+use peluqueria;
+
+-- CONSULTA NUMERO 1 --
+create table dueno(
+	DNI int not null,
+    Nombre varchar(50) not null,
+    Apellido varchar(50) not null,
+    Telefono varchar(50) not null,
+    Direccion varchar(100) not null,
+    PRIMARY KEY (DNI)
+
+);
+
+create table perro(
+	ID_Perro int not null,
+    Nombre varchar(50) not null,
+    Fecha_nac varchar(50) not null,
+    Sexo varchar (30) not null,
+    DNI_dueno int not null,
+    PRIMARY KEY (ID_Perro),
+    FOREIGN KEY (DNI_dueno) REFERENCES dueno (DNI)
+);
+
+create table historial(
+	ID_Historial int not null,
+    Fecha varchar(50) not null,
+    Perro int not null,
+    Descripcion varchar(100) not null,
+    Monto varchar(100) not null,
+    PRIMARY KEY (ID_Historial),
+    FOREIGN KEY (Perro) REFERENCES perro (ID_Perro)
+);
+
+-- CONSULTA NUMERO 2 -- 
+INSERT INTO dueno(DNI, Nombre, Apellido, Telefono, Direccion) VALUES (37956312, "Diego", "Cabrera", "2352505048", "Velez Sarsfield 85");
+INSERT INTO perro(ID_Perro, Nombre, Fecha_nac, Sexo, DNI_dueno) VALUES (1, "Max", "10/8/2018", "Macho", 37956312);
+
+-- CONSULTA NUMERO 4 --
+SELECT * FROM perro;
+UPDATE perro
+SET fecha_nac = "25/3/2020"
+WHERE ID_Perro = 19
